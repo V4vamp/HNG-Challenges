c781abe56ad9d29d1e996effa8d0ab79302f41a8
Muhammad Mukhtar <yusoofjnr@gmail.com>
2023-10-15 18:05:18 +0100

    new commit

--- Stage-1-BE/function/express.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const serverless = require('serverless-http');
-const mainApp = require('../app'); 
-
-const app = express();
-
-app.use('/', mainApp);
-
-module.exports.handler = serverless(app);
